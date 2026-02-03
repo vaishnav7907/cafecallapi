46b4f873cbaa0f14d9131a78fa26cf7e57a406b7
vaishnav <vaishnav790779@gmail.com>
2026-02-03 11:32:10 +0530

    sfff

--- mongoDb/config.js
+++ mongoDb/config.js
@@ -4,7 +4,7 @@ const connection = async ()=>{
 
     try {
       
-        const connect = await mongoose.connect("mongodb+srv://vaishnavk3699_db_user:cafecall123@cluster0.fx4x9jr.mongodb.net/?appName=Cluster0")
+        const connect = await mongoose.connect(process.env.mongouri)
         console.log("Connection Successfully");
         
 
